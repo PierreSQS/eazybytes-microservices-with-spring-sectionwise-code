4aed7348314a1924e841bb898470e63ecf8ee263
PierreSQS <nomo_messina@web.de>
2022-08-14 14:54:28 +0200

    Sec7_Chap47-Updating Cards Microservice: Cards initial commit

---
+++ section7-pierrot/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.eazybytes.cards.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.cards.model.Cards;
+import com.eazybytes.cards.model.Customer;
+import com.eazybytes.cards.repository.CardsRepository;
+
+/**
+ * Extended by Pierrot on 8/14/2022
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class CardsController {
+
+	private final CardsRepository cardsRepository;
+
+	public CardsController(CardsRepository cardsRepository) {
+		this.cardsRepository = cardsRepository;
+	}
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		return cardsRepository.findByCustomerId(customer.getCustomerId());
+	}
+
+}
---
+++ section7-pierrot/cards/src/main/java/com/eazybytes/cards/model/Customer.java
@@ -0,0 +1,10 @@
+package com.eazybytes.cards.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+	private int customerId;
+
+}
