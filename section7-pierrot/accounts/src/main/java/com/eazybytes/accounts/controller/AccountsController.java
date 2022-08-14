4e9c97490407aea2985512b722881a3dee7f5d1b
PierreSQS <nomo_messina@web.de>
2022-08-14 05:36:29 +0200

    Sec7_Chap45-Updating Account Microservice: related code

--- section7-pierrot/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section7-pierrot/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,35 +1,46 @@
 /**
- * 
+ *
  */
 package com.eazybytes.accounts.controller;
 
+import com.eazybytes.accounts.config.AccountServiceConfig;
+import com.eazybytes.accounts.model.Accounts;
+import com.eazybytes.accounts.model.Customer;
+import com.eazybytes.accounts.model.Properties;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eazybytes.accounts.model.Accounts;
-import com.eazybytes.accounts.model.Customer;
-import com.eazybytes.accounts.repository.AccountsRepository;
 
 /**
- * Optimized by Pierrot on 8/12/2022
+ * Optimized by Pierrot on 8/14/2022
  * @author Eazy Bytes
  *
  */
 
 @RestController
 public class AccountsController {
-	
-	private final AccountsRepository accountsRepository;
 
-	public AccountsController(AccountsRepository accountsRepository) {
-		this.accountsRepository = accountsRepository;
-	}
+    private final AccountsRepository accountsRepository;
+    private final AccountServiceConfig accountServConfig;
 
-	@PostMapping("/myAccount")
-	public Accounts getAccountDetails(@RequestBody Customer customer) {
+    public AccountsController(AccountsRepository accountsRepository, AccountServiceConfig accountServConfig) {
+        this.accountsRepository = accountsRepository;
+        this.accountServConfig = accountServConfig;
+    }
 
-		return accountsRepository.findByCustomerId(customer.getCustomerId());
-	}
+    @PostMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+        return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/account/properties")
+    public Properties getPropertiesDetails() {
+        return new Properties(accountServConfig.getMsg(), accountServConfig.getBuildVersion(),
+                accountServConfig.getMailDetails(), accountServConfig.getActiveBranches());
+    }
 
 }
