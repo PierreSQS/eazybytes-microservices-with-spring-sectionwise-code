3ea02c49674d0d6d3631daf288df79d4d9ab2934
PierreSQS <nomo_messina@web.de>
2022-08-17 06:39:35 +0200

    Sec8_Chap64-Feign Client to invoke other µServices: optimized code

--- section8-pierrot/accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
+++ section8-pierrot/accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -1,18 +1,16 @@
 package com.eazybytes.accounts.service.client;
 
-import java.util.List;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.eazybytes.accounts.model.Customer;
 import com.eazybytes.accounts.model.Loans;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient("loans")
 public interface LoansFeignClient {
 
-	@RequestMapping(method = RequestMethod.POST, value = "myLoans", consumes = "application/json")
+	@PostMapping("myLoans")
 	List<Loans> getLoansDetails(@RequestBody Customer customer);
 }
