6214c7901aec8f1c25fbb96b40d22ef508ce2f36
PierreSQS <nomo_messina@web.de>
2022-09-29 23:56:30 +0200

    Sec17_Chap158-Code Changes in Gateway: code related

---
+++ section_17-pierrot/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.eaztbytes.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain (ServerHttpSecurity httpSecurity) {
+
+        httpSecurity.authorizeExchange(exchanges ->
+                        exchanges.pathMatchers("/eazybank/accounts/**").authenticated()
+                                .pathMatchers("/eazybank/cards/**").authenticated()
+                                .pathMatchers("/eazybank/loans/**").permitAll())
+                .oauth2ResourceServer().jwt();
+
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
+
+    }
+
+}
