7b178b5b99ae28c3f111ea7ebe03e9d7ed02ce44
PierreSQS <nomo_messina@web.de>
2022-10-16 15:24:15 +0200

    Sec17_Chap166-Authorization with Keycloak: added a Role-Converter and adapted SecurityConfig

--- section_17-pierrot/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/config/SecurityConfig.java
+++ section_17-pierrot/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/config/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.eaztbytes.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,15 +20,24 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain (ServerHttpSecurity httpSecurity) {
 
         httpSecurity.authorizeExchange(exchanges ->
-                        exchanges.pathMatchers("/eazybank/accounts/**").authenticated()
+                        exchanges.pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
                                 .pathMatchers("/eazybank/cards/**").authenticated()
                                 .pathMatchers("/eazybank/loans/**").permitAll())
                 // sets this server as Oauth2 Resource Server with JWT-Tokens
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt()
+                // sets the jwt-Converter
+                .jwtAuthenticationConverter(grantedAuthoritiesExtractor());
 
         httpSecurity.csrf().disable();
         return httpSecurity.build();
 
+    }
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter  = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
 }
